c7be4ab9bd07f3bc39f99096df03a9abf11a7542
Lubov93 <ru.lubov@gmail.com>
2021-10-18 12:46:29 +0300

    Created STATUS Codes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
 from typing import Optional
+
+from starlette.responses import Response
 
 app = FastAPI()
 
@@ -39,8 +41,13 @@ def get_blog_type(type: BlogType):
     return {'message': f' Blog with id {type}'}
 
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {'message': f'Blog with id {id}'}
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog{id} not found'}
+    else: 
+        response.status_code = status.HTTP_200_OK
+        return {'message': f'Blog with id {id}'}
 
 
