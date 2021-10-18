8b7e71ab8f578a9bc465cb08328e076f5df45660
Lubov93 <ru.lubov@gmail.com>
2021-10-18 15:16:25 +0300

    Query params

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from router import blog_get
+from router import blog_post
 
 
 from starlette.responses import Response
 
 app = FastAPI()
 app.include_router(blog_get.router)
+app.include_router(blog_post.router)
 
 @app.get('/')
 def index():
