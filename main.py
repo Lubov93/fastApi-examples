8afd9278a2c2df831f79214087c4a18d45876fe6
Lubov93 <ru.lubov@gmail.com>
2021-10-18 13:59:18 +0300

    Added routers and new settings for API

--- main.py
+++ main.py
@@ -1,66 +1,18 @@
-from fastapi import FastAPI, status, Response
-from enum import Enum
-from typing import Optional
+from fastapi import FastAPI
+from router import blog_get
+
 
 from starlette.responses import Response
 
 app = FastAPI()
+app.include_router(blog_get.router)
 
-# @app.get('/')
-# def index():
-#     return {'message': 'Hello world!'}
+@app.get('/')
+def index():
+    return {'message': 'Hello world!'}
 
 
 # @app.post('/hi')
 # def index2():
 #     return 'Hi'
-
-
-# @app.get('/blog/all')
-# def get_all_blogs():
-#     return {'message': 'All blogs provided'}
-
-@app.get(
-    '/blog/all',
-    tags=['blog'],
-    description='This api call simullate fetching all blogsl'
-    )
-def get_all_blogs(page =1 , page_size: Optional[int]=None):
-    return {'message', f'All {page_size} blogs on page {page}'}
-
-
-@app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
-def get_comment(id: int, comment_id: int, valid: bool=True, username: Optional[str]= None):
-    """
-    Simulates retrieving a comment of blog
-
-    -**id** mandatory path parameter
-    - **comment_id** mandatory path parameter
-    - **valid** optional query parameter
-    - **username** optional query parameter
-    
-    """
-    return {'message': f'blog_id{id}, comment_id{comment_id}, valid{valid}, username{username}'}
-
-
-class BlogType(str, Enum):
-    short = 'short'
-    story = 'story'
-    howto = 'howto'
-
-
-@app.get('/blog/type/{type}')
-def get_blog_type(type: BlogType):
-    return {'message': f' Blog with id {type}'}
-
-
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
-def get_blog(id: int, response: Response):
-    if id > 5:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {'error': f'Blog{id} not found'}
-    else: 
-        response.status_code = status.HTTP_200_OK
-        return {'message': f'Blog with id {id}'}
-
 
