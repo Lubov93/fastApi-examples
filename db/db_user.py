5a1bddba0fc9e5048f42ae37df9356eecfd266c9
Lubov93 <ru.lubov@gmail.com>
2021-10-19 21:31:32 +0300

    Process of creation of new User and created for him Hashed password

---
+++ db/db_user.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm.session import Session
+from shemas import UserBase
+from db.models import DbUser
+
+def create_user(db: Session, request: UserBase):
+    new_user = DbUser(
+        username = request.username,
+        email = request.email,
+        password = Hash(request.password)
+    ) # from models.py look
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user) # Refresh because id reason for new_user
+    return new_user
+
